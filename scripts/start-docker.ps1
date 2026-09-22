049004f1f8c847abd99ada21e8da1d17d2b776ea
Matanya Vinograd <Matanya_Vinograd@epam.com>
2026-09-22 20:22:48 +0300

    chore: add Windows auto-start script for docker compose

---
+++ scripts/start-docker.ps1
@@ -0,0 +1,4 @@
+﻿# Auto-start recruitment system Docker containers on Windows login
+Start-Sleep -Seconds 20  # wait for Docker Desktop to be ready
+Set-Location "C:\Users\MatanyaVinograd\my_project\recruitment_system"
+docker compose up -d 2>&1 | Out-File ".docker-startup.log" -Encoding UTF8
